274b579f36d1a7862b4afe0b6ae874a9871c9f41
gary <gary@MACSTATION.local>
2020-07-05 21:27:36 +0100

    Updates

--- spa/webpack/webpack.config.dev.js
+++ spa/webpack/webpack.config.dev.js
@@ -1,6 +1,6 @@
 
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const baseConfig = require('./webpack.config.base.js');
 
 module.exports = merge(baseConfig, {
