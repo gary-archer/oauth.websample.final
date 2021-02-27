b4856b31e7168c6c45f23f55270339c4f134caca
Gary Archer <garyarcher36@gmail.com>
2021-02-27 22:49:56 +0000

    Updates

--- spa/dist/unsupported.js
+++ spa/dist/unsupported.js
@@ -1,14 +1,13 @@
 /*
- * Handle older browsers such as Internet Explorer gracefully
+ * Provide an unsupported message if Internet Explorer is used
  */
-var root = document.getElementById('root');
-var html = '<div style="align-items: center; padding: 20px; font-family: sans-serif; font-size: 20px;">';
+var html = '<div style="font-size: 20px; align-items: center; padding: 20px">';
 html += '<ul>';
-html += '<li style="list-style:none"><strong>This browser is not supported, please use one of the following:</strong></li>';
-html += '<li style="margin: 10px; list-style-position: inside"">Firefox</li>';
-html += '<li style="margin: 10px; list-style-position: inside"">Edge</li>';
-html += '<li style="margin: 10px; list-style-position: inside"">Chrome</li>';
-html += '<li style="margin: 10px; list-style-position: inside"">Safari</li>';
+html += '<li style="list-style: none; font-weight: bold">This browser is not supported, please use one of the following:</li>';
+html += '<li style="list-style: inside; padding: 5px">Firefox</li>';
+html += '<li style="list-style: inside; padding: 5px">Edge</li>';
+html += '<li style="list-style: inside; padding: 5px">Chrome</li>';
+html += '<li style="list-style: inside; padding: 5px">Safari</li>';
 html += '</ul>';
 html += '</div>';
-root.innerHTML = html;
+document.getElementById('root').innerHTML = html;
