260dc377771ef6f05934d7bcf22c66771ac38781
gary <gary@MACSTATION.local>
2021-09-13 19:13:07 +0100

    Updates

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+##############################################################################
+# Run the SPA locally, routing API requests via a local Docker Compose network
+##############################################################################
+
+#
+# Call some logic from the localhost folder
+#
---
+++ runWithLocalApi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#############################################################################
+# Run all of the SPA, BFF API and Business API locally, using different ports
+#############################################################################
+
+#
+# Call some logic from the localhost folder
+#
