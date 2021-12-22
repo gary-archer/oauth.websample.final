d704208014ccd845b274e72e92c8ca5c64d7cdf5
gary <gary@MACSTATION.local>
2021-12-22 21:45:16 +0000

    Updates

--- test/runHttpWorkflow.sh
+++ test/runHttpWorkflow.sh
@@ -4,12 +4,12 @@
 # A script to test the SPA's HTTP workflow against deployed APIs
 ################################################################
 
-#WEB_BASE_URL='https://web.mycompany.com'
-#TOKEN_HANDLER_BASE_URL='https://api.mycompany.com:444/tokenhandler'
-#BUSINESS_API_BASE_URL='https://api.mycompany.com:444/api'
-WEB_BASE_URL='https://web.authsamples.com'
-TOKEN_HANDLER_BASE_URL='https://api.authsamples.com/tokenhandler'
-BUSINESS_API_BASE_URL='https://api.authsamples.com/api'
+WEB_BASE_URL='https://web.mycompany.com'
+TOKEN_HANDLER_BASE_URL='https://api.mycompany.com:444/tokenhandler'
+BUSINESS_API_BASE_URL='https://api.mycompany.com:444/api'
+#WEB_BASE_URL='https://web.authsamples.com'
+#TOKEN_HANDLER_BASE_URL='https://api.authsamples.com/tokenhandler'
+#BUSINESS_API_BASE_URL='https://api.authsamples.com/api'
 LOGIN_BASE_URL='https://login.authsamples.com'
 COOKIE_PREFIX=mycompany
 TEST_USERNAME='guestuser@mycompany.com'
