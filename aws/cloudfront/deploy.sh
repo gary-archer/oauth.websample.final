b83e15d48062911822f463d5920ca079eb1f4ff0
gary <gary@MACSTATION.local>
2022-03-28 08:34:06 +0100

    Updates

--- aws/cloudfront/deploy.sh
+++ aws/cloudfront/deploy.sh
@@ -14,4 +14,4 @@ aws s3 cp .package s3://web.authsamples.com --recursive
 #
 # Invalidate existing files
 #
-aws cloudfront create-invalidation --distribution-id=E1P4XPOL1PNE6Z --paths /*
+aws cloudfront create-invalidation --distribution-id=E1P4XPOL1PNE6Z --paths '/*'
