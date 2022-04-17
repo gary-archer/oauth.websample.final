31840894c5b707635835a1077db0f4eb7fac4ca3
gary <gary@MACSTATION.local>
2022-04-17 11:36:42 +0100

    Updates

--- spa/src/api/entities/userInfo.ts
+++ spa/src/api/entities/userInfo.ts
@@ -1,7 +1,12 @@
 /*
- * User info returned to the UI from the API is not limited to OAuth data
+ * Any user info can be returned to the UI
  */
 export interface UserInfo {
+
+    // These values originate from OAuth user info
     givenName: string;
     familyName: string;
+
+    // This value originates from the API's own data
+    regions: string[];
 }
