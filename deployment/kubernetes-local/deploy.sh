d1e6ab8ab011b5bb73df6ae80b15abf8258bda9a
gary <gary@MACSTATION.local>
2022-08-30 17:44:55 +0100

    Updates

--- deployment/kubernetes-local/deploy.sh
+++ deployment/kubernetes-local/deploy.sh
@@ -18,8 +18,8 @@ cp ../environments/webhost/kubernetes-local.config.json webhost.config.json
 #
 # Create a configmap for the SPA's JSON configuration file
 #
-kubectl -n deployed delete configmap spa-config 2>/dev/null
-kubectl -n deployed create configmap spa-config --from-file=spa.config.json
+kubectl -n applications delete configmap spa-config 2>/dev/null
+kubectl -n applications create configmap spa-config --from-file=spa.config.json
 if [ $? -ne 0 ]; then
   echo '*** Problem encountered creating the SPA configmap'
   exit 1
@@ -28,8 +28,8 @@ fi
 #
 # Create a configmap for the Web Host's JSON configuration file
 #
-kubectl -n deployed delete configmap webhost-config 2>/dev/null
-kubectl -n deployed create configmap webhost-config --from-file=webhost.config.json
+kubectl -n applications delete configmap webhost-config 2>/dev/null
+kubectl -n applications create configmap webhost-config --from-file=webhost.config.json
 if [ $? -ne 0 ]; then
   echo '*** Problem encountered creating the Web Host configmap'
   exit 1
@@ -38,8 +38,8 @@ fi
 #
 # Create a secret for the private key password of the certificate file cert-manager will create
 #
-kubectl -n deployed delete secret webhost-pkcs12-password 2>/dev/null
-kubectl -n deployed create secret generic webhost-pkcs12-password --from-literal=password='Password1'
+kubectl -n applications delete secret webhost-pkcs12-password 2>/dev/null
+kubectl -n applications create secret generic webhost-pkcs12-password --from-literal=password='Password1'
 if [ $? -ne 0 ]; then
   echo '*** Problem encountered creating the Web Host certificate secret'
   exit 1
@@ -48,8 +48,8 @@ fi
 #
 # Trigger deployment of the web host to the Kubernetes cluster
 #
-kubectl -n deployed delete -f webhost.yaml 2>/dev/null
-kubectl -n deployed apply  -f webhost.yaml
+kubectl -n applications delete -f webhost.yaml 2>/dev/null
+kubectl -n applications apply  -f webhost.yaml
 if [ $? -ne 0 ]; then
   echo '*** Web Host Kubernetes deployment problem encountered'
   exit 1
