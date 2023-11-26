507ebf44b33abf347701b65cb3612b9af70b7671
gary <garyarcher36@gmail.com>
2023-11-26 20:02:28 +0000

    Updates

--- spa/src/configuration/productionConfiguration.ts
+++ spa/src/configuration/productionConfiguration.ts
@@ -2,6 +2,7 @@ import {Configuration} from './configuration';
 
 /*
  * This deployment points to Serverless APIs
+ * Due to AWS API gateway limitations the SPA routes to the API via a /tokenhandler path
  */
 export const productionServerlessConfiguration = {
     apiBaseUrl: 'https://api.authsamples.com/tokenhandler/investments',
@@ -10,8 +11,9 @@ export const productionServerlessConfiguration = {
 
 /*
  * This deployment points to APIs running a Kubernetes cluster
+ * The cloud native API gateway does cookie processing then forwards a JWT access token to APIs
  */
 export const productionCloudNativeConfiguration = {
-    apiBaseUrl: 'https://api.authsamples-k8s.com/tokenhandler/investments',
-    oauthAgentBaseUrl: 'https://api.authsamples-k8s.com/tokenhandler/oauth-agent',
+    apiBaseUrl: 'https://api.authsamples-k8s.com/investments',
+    oauthAgentBaseUrl: 'https://api.authsamples-k8s.com/oauth-agent',
 } as Configuration;
