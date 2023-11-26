60ee497047e60332037a31b1ae043ddb1f0b61b1
gary <garyarcher36@gmail.com>
2023-11-26 11:01:57 +0000

    Updates

--- spa/src/configuration/productionConfiguration.ts
+++ spa/src/configuration/productionConfiguration.ts
@@ -4,14 +4,14 @@ import {Configuration} from './configuration';
  * This deployment points to Serverless APIs
  */
 export const productionServerlessConfiguration = {
-    apiBaseUrl: 'https://api.authsamples.com/tokenhandler/investments',
-    oauthAgentBaseUrl: 'https://api.authsamples.com/tokenhandler/oauth-agent',
+    apiBaseUrl: 'https://api.authsamples.com/demobrand/investments',
+    oauthAgentBaseUrl: 'https://api.authsamples.com/demobrand/oauth-agent',
 } as Configuration;
 
 /*
  * This deployment points to APIs running a Kubernetes cluster
  */
 export const productionCloudNativeConfiguration = {
-    apiBaseUrl: 'https://api.authsamples-k8s.com/tokenhandler/investments',
-    oauthAgentBaseUrl: 'https://api.authsamples-k8s.com/tokenhandler/oauth-agent',
+    apiBaseUrl: 'https://api.authsamples-k8s.com/demobrand/investments',
+    oauthAgentBaseUrl: 'https://api.authsamples-k8s.com/demobrand/oauth-agent',
 } as Configuration;
