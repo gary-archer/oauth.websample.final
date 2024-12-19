c4647f61218bf628d5da6d5e076b4226bb18fa06
gary-archer <garyarcher36@gmail.com>
2024-12-19 19:42:24 +0000

    Updates

--- webpack/webpack.config.dev.ts
+++ webpack/webpack.config.dev.ts
@@ -4,10 +4,15 @@ import path from 'path';
 import webpack from 'webpack';
 import {merge} from 'webpack-merge';
 import baseConfig from './webpack.config.base.js';
-import webpackDevServer from 'webpack-dev-server';
+import webpackDevServer, {Request, Response, NextFunction} from 'webpack-dev-server';
 
+/*
+ * Configure the main development server
+ */
 const dirname = process.cwd();
 const devServer: webpackDevServer.Configuration = {
+
+    // Use HTTPS for local development
     server: {
         type: 'https',
         options: {
@@ -20,12 +25,34 @@ const devServer: webpackDevServer.Configuration = {
     },
     port: 443,
     historyApiFallback: {
+        // Serve the index.html file for this subfolder for not found routes like /spa/xxx
         index: '/spa/',
     },
     hot: true,
     allowedHosts: [
         'www.authsamples-dev.com',
     ],
+    setupMiddlewares: (middlewares: webpackDevServer.Middleware[]) => {
+
+        // Redirect for paths outside the root, which React Router cannot serve when it uses a base path
+        // - https://webpack.js.org/configuration/dev-server/#devserversetupmiddlewares
+        middlewares.unshift({
+            name: 'first-in-array',
+            path: '/',
+            middleware: (request: Request, response: Response, next: NextFunction) => {
+
+                const currentPath = request.path.toLowerCase();
+                if (currentPath.startsWith('/spa/') || currentPath === '/favicon.ico') {
+                    next();
+                    return;
+                }
+
+                response.redirect('/spa/');
+            },
+        });
+
+        return middlewares;
+    },
 };
 
 const devConfig: webpack.Configuration = {
