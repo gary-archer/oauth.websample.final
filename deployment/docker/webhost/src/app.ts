2b1be59ef02b543d72b8dc4f98b2fbe79fea5cc1
gary <garyarcher36@gmail.com>
2025-12-31 17:24:47 +0000

    Updates

--- deployment/docker/webhost/src/app.ts
+++ deployment/docker/webhost/src/app.ts
@@ -1,3 +1,4 @@
+import compression from 'compression';
 import express from 'express';
 import fs from 'node:fs/promises';
 import {Configuration} from './configuration.js';
@@ -7,9 +8,10 @@ import {HttpServerConfiguration} from './httpServerConfiguration.js';
 const configurationJson = await fs.readFile('webhost.config.json', 'utf-8');
 const configuration =  JSON.parse(configurationJson) as Configuration;
 
-// Create the web host
-const expressApp = express();
-const httpServer = new HttpServerConfiguration(expressApp, configuration);
+// Create the web host and enable compression
+const app = express();
+app.use(compression());
+const httpServer = new HttpServerConfiguration(app, configuration);
 
 // Set up web static content delivery
 httpServer.initialiseWebStaticContentHosting();
