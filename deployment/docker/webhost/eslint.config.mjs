3b8e34707d0eb805d3debb70afcaafb200443a25
gary <garyarcher36@gmail.com>
2026-01-11 10:06:17 +0000

    Updates

--- deployment/docker/webhost/eslint.config.mjs
+++ deployment/docker/webhost/eslint.config.mjs
@@ -1,25 +1,28 @@
 
-import eslint from '@eslint/js';
+import {defineConfig, globalIgnores} from 'eslint/config'
+import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-    extends: [
-        eslint.configs.recommended,
-        ...tseslint.configs.strict,
-    ],
-    files: ['**/*.ts'],
-    rules: {
-        '@typescript-eslint/explicit-module-boundary-types': ['error', {
-            'allowArgumentsExplicitlyTypedAsAny': true,
-        }],
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-extraneous-class': 'off',
-        'brace-style': ['error', '1tbs'],
-        'indent': 'error',
-        'max-len': ['error', { 'code': 120 }],
-        'no-multiple-empty-lines': ['error', {'max': 1}],
-        'no-trailing-spaces': 'error',
-        'quotes': ['error', 'single', { 'avoidEscape': true }],
-        'semi': 'error',
+export default defineConfig([
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.strict,
+        ],
+        rules: {
+            'brace-style': ['error', '1tbs'],
+            'max-len': ['error', { 'code': 120 }],
+            'no-multiple-empty-lines': ['error', {'max': 1}],
+            'no-trailing-spaces': 'error',
+            'quotes': ['error', 'single', { 'avoidEscape': true }],
+            'semi': 'error',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-extraneous-class': 'off',
+            '@typescript-eslint/explicit-module-boundary-types': ['error', {
+                'allowArgumentsExplicitlyTypedAsAny': true,
+            }],
+        },
     },
-});
+]);
--- spa/eslint.config.mjs
+++ spa/eslint.config.mjs
@@ -1,37 +1,39 @@
 
-import eslint from '@eslint/js';
+import {defineConfig, globalIgnores} from 'eslint/config'
+import js from '@eslint/js';
 import react from 'eslint-plugin-react';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-    extends: [
-        eslint.configs.recommended,
-        ...tseslint.configs.strict,
-    ],
-    files: [
-        '**/*.ts','**/*.tsx',
-    ],
-    plugins: {
-        react,
-    },
-    rules: {
-        'brace-style': ['error', '1tbs'],
-        'max-len': ['error', { 'code': 120 }],
-        'no-multiple-empty-lines': ['error', {'max': 1}],
-        'no-trailing-spaces': 'error',
-        'quotes': ['error', 'single', { 'avoidEscape': true }],
-        'semi': 'error',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-extraneous-class': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': ['error', {
-            'allowArgumentsExplicitlyTypedAsAny': true,
-        }],
-        ... react.configs.recommended.rules,
-        'react/react-in-jsx-scope': 'off',
-    },
-    settings: {
-        react: {
-            version: 'detect',
+export default defineConfig([
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.strict,
+        ],
+        rules: {
+            'brace-style': ['error', '1tbs'],
+            'max-len': ['error', { 'code': 120 }],
+            'no-multiple-empty-lines': ['error', {'max': 1}],
+            'no-trailing-spaces': 'error',
+            'quotes': ['error', 'single', { 'avoidEscape': true }],
+            'semi': 'error',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-extraneous-class': 'off',
+            '@typescript-eslint/explicit-module-boundary-types': ['error', {
+                'allowArgumentsExplicitlyTypedAsAny': true,
+            }],
+            ... react.configs.recommended.rules,
+            'react/react-in-jsx-scope': 'off',
+        },
+        plugins: {
+            react,
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
         },
     },
-});
+]);
