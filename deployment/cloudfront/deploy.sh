b8ad344579551ccdff6d1cd5d7e601dcb793ad46
gary-archer <garyarcher36@gmail.com>
2026-05-26 21:29:11 +0100

    Updates

--- deployment/cloudfront/deploy.sh
+++ deployment/cloudfront/deploy.sh
@@ -31,6 +31,7 @@ fi
 #
 # Deploy the SPA's static content to the upload point for distribution
 #
+echo 'Deploying files to S3 ...'
 aws s3 cp .package "s3://$DOMAIN" --recursive
 if [ $? -ne 0 ]; then
   echo 'Problem encountered uploading web static content to S3'
@@ -40,8 +41,25 @@ fi
 #
 # Invalidate the index.html in the CloudFront cache so that users get the new one when they next reload the page
 #
+echo 'Invalidating the index.html file ...'
 aws cloudfront create-invalidation --distribution-id="$DISTRIBUTION_ID" --paths '/spa/index.html'
 if [ $? -ne 0 ]; then
   echo 'Problem encountered creating the CloudFront invalidation'
   exit
 fi
+
+#
+# Delete CDN files with old chunk names
+#
+echo 'Deleting old CDN files ...'
+if [ "$(uname -s)" == 'Darwin' ]; then
+  CUTOFF=$(date -v-7d +%F)
+else
+  CUTOFF=$(date -d '7 days ago' +%F)
+fi
+
+aws s3 ls s3://$DOMAIN/spa --recursive \
+  | awk -v cutoff="$CUTOFF" '$1 < cutoff {print $4}' \
+  | xargs -I {} aws s3 rm "s3://$DOMAIN/{}"
+
+aws s3 ls s3://$DOMAIN/spa --recursive
