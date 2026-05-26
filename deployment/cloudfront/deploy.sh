10b40b4266ac2eb1f303afbd04fb940c91b46ba9
gary-archer <garyarcher36@gmail.com>
2026-05-26 21:01:02 +0100

    Updates

--- deployment/cloudfront/deploy.sh
+++ deployment/cloudfront/deploy.sh
@@ -18,32 +18,29 @@ fi
 #
 # Deployment supports two scenarios, where the SPA connects to APIs that use either Serverless or Kubernetes hosting
 #
-ENVIRONMENT='serverless'
 if [ "$1" == 'cloudnative' ]; then
   ENVIRONMENT='cloudnative'
+  DOMAIN='www.authsamples-k8s.com'
+  DISTRIBUTION_ID='E3ADD9C1K3LQWL'
+else 
+  ENVIRONMENT='serverless'
+  DOMAIN='www.authsamples.com'
+  DISTRIBUTION_ID='E1P4XPOL1PNE6Z'
 fi
 
 #
 # Deploy the SPA's static content to the upload point for distribution
 #
-if [ "$ENVIRONMENT" == 'serverless' ]; then
-  aws s3 cp .package s3://www.authsamples.com --recursive
-else
-  aws s3 cp .package s3://www.authsamples-k8s.com --recursive
-fi
+aws s3 cp .package "s3://$DOMAIN" --recursive
 if [ $? -ne 0 ]; then
   echo 'Problem encountered uploading web static content to S3'
   exit
 fi
 
 #
-# For our simple deployment, invalidate all files in the CDN to force global redistribution
+# Invalidate the index.html
 #
-if [ "$ENVIRONMENT" == 'serverless' ]; then
-  aws cloudfront create-invalidation --distribution-id=E1P4XPOL1PNE6Z --paths '/*'
-else
-  aws cloudfront create-invalidation --distribution-id=E3ADD9C1K3LQWL --paths '/*'
-fi
+aws cloudfront create-invalidation --distribution-id="$DISTRIBUTION_ID" --paths '/spa/index.html'
 if [ $? -ne 0 ]; then
   echo 'Problem encountered creating a Cloudfront invalidation'
   exit
